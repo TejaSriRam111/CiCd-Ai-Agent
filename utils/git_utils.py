ae88e64c770f09873da2bce2dfe967daf22a7b3f
BILLURI TEJA SRI RAM <164980774+TejaSriRam111@users.noreply.github.com>
2026-01-29 14:09:22 +0530

    Update git_utils.py

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -1,10 +1,18 @@
 from git import Repo
 import os
 import shutil
+import stat
+
+def force_remove(func, path, exc_info):
+    """
+    Handle Windows permission errors when deleting files
+    """
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
 
 def clone_repo(repo_url, dest="repos/temp"):
     if os.path.exists(dest):
-        shutil.rmtree(dest)
+        shutil.rmtree(dest, onerror=force_remove)
+
     Repo.clone_from(repo_url, dest)
     return dest
- 
