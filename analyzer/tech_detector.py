2d5266c3a386b43c23a0741aa3bf38c0821cc868
BILLURI TEJA SRI RAM <164980774+TejaSriRam111@users.noreply.github.com>
2026-01-29 14:04:11 +0530

    Update tech_detector.py

--- analyzer/tech_detector.py
+++ analyzer/tech_detector.py
@@ -1,38 +1,29 @@
 import os
 
+def get_all_files(repo_path):
+    file_list = []
+    for root, dirs, files in os.walk(repo_path):
+        for file in files:
+            file_list.append(file)
+    return file_list
+
+
 def detect_tech(repo_path):
-    files = os.listdir(repo_path)
+    files = get_all_files(repo_path)
 
     if "pom.xml" in files:
-        return {
-            "language": "java",
-            "build_tool": "maven",
-            "ci": "github-actions"
-        }
+        return {"language": "java", "build_tool": "maven", "ci": "github-actions"}
 
     if "build.gradle" in files:
-        return {
-            "language": "java",
-            "build_tool": "gradle",
-            "ci": "github-actions"
-        }
+        return {"language": "java", "build_tool": "gradle", "ci": "github-actions"}
 
     if "package.json" in files:
-        return {
-            "language": "node",
-            "build_tool": "npm",
-            "ci": "github-actions"
-        }
+        return {"language": "node", "build_tool": "npm", "ci": "github-actions"}
 
     if "requirements.txt" in files:
-        return {
-            "language": "python",
-            "build_tool": "pip",
-            "ci": "github-actions"
-        }
+        return {"language": "python", "build_tool": "pip", "ci": "github-actions"}
 
-    return {
-        "language": "unknown",
-        "build_tool": "unknown",
-        "ci": "github-actions"
-    }
+    if "index.html" in files:
+        return {"language": "html", "build_tool": "none", "ci": "github-actions"}
+
+    return {"language": "unknown", "build_tool": "unknown", "ci": "github-actions"}
