89aca79b3cd138bef96a3ff2d6c459c21ec93676
BILLURI TEJA SRI RAM <164980774+TejaSriRam111@users.noreply.github.com>
2026-01-30 16:20:37 +0530

    Create cicd_agent.py

---
+++ agent/cicd_agent.py
@@ -0,0 +1,14 @@
+from analyzer.repo_analyzer import analyze_repo
+from agent.planner import plan_pipeline
+from generator.workflow_writer import write_workflow
+
+class CICDAgent:
+    def __init__(self, llm):
+        self.llm = llm
+
+    def run(self, repo_path, mode="full-devops"):
+        repo_context = analyze_repo(repo_path)
+        workflow_yaml = plan_pipeline(repo_context, self.llm, mode)
+        write_workflow(repo_path, workflow_yaml)
+
+        print("✅ CI/CD generated and deployment started")
