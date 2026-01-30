991e51014770bddc7b48db6cb58eea3a509aaf12
BILLURI TEJA SRI RAM <164980774+TejaSriRam111@users.noreply.github.com>
2026-01-30 16:45:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
 from agent.cicd_agent import CICDAgent
 from llm.claude import ask_claude
 
 def main():
+    repo_path = sys.argv[1]
     agent = CICDAgent(llm=ask_claude)
-    agent.run("./target-repo", mode="full-devops")
+    agent.run(repo_path, mode="full-devops")
 
 if __name__ == "__main__":
     main()
