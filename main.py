e446ddf40ccc2e40b953e8c171651ed711a90bfa
BILLURI TEJA SRI RAM <164980774+TejaSriRam111@users.noreply.github.com>
2026-02-04 10:56:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from agent.cicd_agent import CICDAgent
 from llm.claude import ask_claude
-from tools.repo_resolver import resolve_repo
+from utils.repo_resolver import resolve_repo
 
 def main():
     print("🤖 CI/CD AI Agent Started")
-    
+
     repo_url = input("👉 Enter Git repository URL: ").strip()
     if not repo_url:
-        print("❌ Repo URL cannot be empty")
+        print("❌ Repository URL cannot be empty")
         return
 
     repo_path = resolve_repo(repo_url)
